bd309b69c5d2a2b014aff9a42060dc4c73f82c08
Kabeer-kumar04 <83095060+Kabeer-kumar04@users.noreply.github.com>
2021-07-13 11:55:18 +0500

    Update App.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 alert("Welcome to Our Page of tourism page\n We provide the best we can");
+
+function signup(){
+  var a=document.getelementbyid("signup");
+  var a= "something went wrong";
+  alert(a);
+}
+signup();
+
