5cafaea6ae2f008dd5c2cab8e12a9e91ffe0fa3e
kabeer kumar <kabeerkumar908@gmail.com>
2021-07-13 11:58:40 +0500

    changes for alert

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 alert("Welcome to Our Page of tourism page\n We provide the best we can");
 
-function signup(){
-  var a=document.getelementbyid("signup");
-  var a= "something went wrong";
-  alert(a);
+function signup() {
+    alert("it will take a few seconds")
 }
-signup();
-
